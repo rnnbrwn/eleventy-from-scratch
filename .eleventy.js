5cf6cd4d47f5df49341918bcaa001bba546ba48c
Ronnie Brown <91823+rnnbrwn@users.noreply.github.com>
2021-05-04 14:47:38 +0100

    Completed course

---
+++ .eleventy.js
@@ -0,0 +1,70 @@
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
+
+// Filters
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
+
+// Transforms
+const htmlMinTransform = require('./src/transforms/html-min-transform.js');
+
+// Create a helpful production flag
+const isProduction = process.env.NODE_ENV === 'production';
+
+
+module.exports = config => {
+
+    // Add filters
+    config.addFilter('dateFilter', dateFilter);
+    config.addFilter('w3DateFilter', w3DateFilter);
+
+    // Set directories to pass through to the dist folder
+    // config.addPassthroughCopy('./src/images/');
+
+    // Only minify HTML if we are in production because it slows builds _right_ down
+    if (isProduction) {
+        config.addTransform('htmlmin', htmlMinTransform);
+    }  
+
+    // Plugins
+    config.addPlugin(rssPlugin);
+
+    const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+
+    // Returns work items, sorted by display order
+    config.addCollection('work', collection => {
+        return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+    });
+    
+    // Returns work items, sorted by display order then filtered by featured
+    config.addCollection('featuredWork', collection => {
+        return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+        x => x.data.featured
+        );
+    });
+
+    // Returns a collection of blog posts in reverse date order
+    config.addCollection('blog', collection => {
+        return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
+    });
+
+    // Returns a list of people ordered by filename
+    config.addCollection('people', collection => {
+        return collection.getFilteredByGlob('./src/people/*.md').sort((a, b) => {
+        return Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1;
+        });
+    });
+  
+    // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
+    config.setUseGitIgnore(false);
+
+    return {
+        markdownTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dir: {
+            input: 'src',
+            output: 'dist'
+        }
+    };
+  };
+  
---
+++ src/filters/w3-date-filter.js
@@ -0,0 +1,6 @@
+module.exports = value => {
+    const dateObject = new Date(value);
+  
+    return dateObject.toISOString();
+  };
+  
